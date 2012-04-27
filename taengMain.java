78ee3d153984d8a7cee25c7ea54b8bdd5f9e077d
Aron Puddy-Mathew <aronpm65536@gmail.com>
2012-04-27 18:37:37 +0930

    first commit

--- taengMain.java
+++ taengMain.java
@@ -2,15 +2,8 @@ public class taengMain {
 	public static void main(String[] args) throws Exception {
 		taeng taengbot = new taeng();
 		taengbot.setVerbose(true);
-		taengbot.connect("irc.freenode.net");
-		taengbot.joinChannel("#aronpmlab");
+		taengbot.connect("");
+		taengbot.joinChannel("");
 		taengbot.setMessageDelay(100);
-		//System.out.println(tell.time(System.currentTimeMillis() - 1335508772360d));
-		// Timing testing
-		/*
-		double start = System.currentTimeMillis();
-		System.out.println((System.currentTimeMillis() - start) + "ms");
-		System.out.println(System.currentTimeMillis());
-		*/
 	}
 }
